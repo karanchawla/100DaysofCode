a14f256f86484aef3444d8fd87a17f8b714cfb3c
Karan Chawla <karangc2@illinois.edu>
2017-09-19 18:15:05 -0500

    Adding unit tests

--- day8/isUnique.py
+++ day8/isUnique.py
@@ -1,8 +1,10 @@
 from collections import Counter
+import unittest
 
 def isUnique(s):
 	s = s.lower()
 	dicty = dict(Counter(s))
+
 	for char in dicty.keys():
 		if dicty[char] > 1:
 			return False
@@ -11,6 +13,7 @@ def isUnique(s):
 
 def isUniqueBrute(s):
 	s = s.lower()
+
 	for i in range(len(s)):
 		for j in range(i+1,len(s)):
 			if(s[j] == s[i]):
@@ -21,12 +24,27 @@ def isUniqueBrute(s):
 def isUniqueOptim(s):
 	s = s.lower()
 	s = sorted(s)
+
 	for i in range(len(s)-1):
 		if(s[i]==s[i+1]):
 			return False
 
 	return True
 
-s = "abcdefgh"
+class Test(unittest.TestCase):
 
-print(isUniqueOptim(s))
+	dataT = [('abcd'), ('s4fad'), ('')]
+	dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+	def test_unique(self):
+		# true check
+		for test_string in self.dataT:
+			actual = isUnique(test_string)
+			self.assertTrue(actual)
+        # false check
+		for test_string in self.dataF:
+			actual = isUnique(test_string)
+			self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()
