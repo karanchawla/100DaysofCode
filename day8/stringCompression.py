bc40b4c25c4f45d8d1e75963bb84fad2e1008762
Karan Chawla <karanchawla.ck@gmail.com>
2017-09-20 00:48:04 -0500

    Past 12, should ideally be in day 9

---
+++ day8/stringCompression.py
@@ -0,0 +1,25 @@
+import unittest
+from collections import Counter
+
+def stringCompression(str1):
+    dicty = dict(Counter(str1))
+    result = []
+    for x in sorted(dicty.keys()):
+        temp = x + str(dicty[x])
+        result.append(temp)
+    result = ''.join(result)
+    if(len(result)>len(str1)):
+        return str1
+    return result
+
+class Test(unittest.TestCase):
+
+    data = [('aabbccddd', 'a2b2c2d3'),('abc','abc')]
+
+    def test_stringCompression(self):
+        for test,solution in self.data:
+            result = stringCompression(test)
+            self.assertEqual(result,solution)
+
+if __name__ == "__main__":
+    unittest.main()
